cf6d7f19ba41649af90d0eade5b37978556ef7b8
jessica-lau <jessica.lau2@gmail.com>
2021-01-12 22:10:07 -0500

    work thru coding problems

---
+++ practice-lvl2.js
@@ -0,0 +1,10 @@
+// Given a string, create a function to reverse the case. All lower-cased letters should be upper-cased, and vice versa.
+
+function reverseCase(phrase) {
+  var i;
+  for (i = 0; i < phrase.length; i++) {
+      if { }
+      else 
+      { }
+  }
+}
