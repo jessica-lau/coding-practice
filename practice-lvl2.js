6695a17651af578fef5671268b1c66ca6fafc609
jessica-lau <jessica.lau2@gmail.com>
2021-01-13 22:43:14 -0500

    work thru practice code

--- practice-lvl2.js
+++ practice-lvl2.js
@@ -1,10 +1,70 @@
-// Given a string, create a function to reverse the case. All lower-cased letters should be upper-cased, and vice versa.
+//Given a string, create a function to reverse the case. All lower-cased letters should be upper-cased, and vice versa.
+
+//pseudo
+//go through each letter of the phrase
+//if letter is uppercase then change to lowercase
+//if letter is lowercase then change to uppercase
 
 function reverseCase(phrase) {
+  var result = "";
   var i;
   for (i = 0; i < phrase.length; i++) {
-      if { }
-      else 
-      { }
+    let letter = phrase.charAt(i);
+    if (letter === letter.toUpperCase()) {
+      result = result.concat(letter.toLowerCase());
+    } else {
+      result = result.concat(letter.toUpperCase());
+    }
+    console.log(phrase[i]);
   }
+
+  return result;
 }
+
+console.log(reverseCase("HAppY"));
+
+//Create a function that prints 1 to a limit and the number should be divisible by 5.
+
+//create a variable to return the number
+//go through each number from 1 to a limit
+//if the number is divisible by 5 then save to the variable
+//if the number is not divisible by 5 then do not save to the variable
+
+//assert expected == actuals
+//assert [] === divisibleFive(1)
+//assert [5] === divisibleFive(5)
+//assert [5, 10] === divisibleFive(10)
+
+function divisibleFive(limit) {
+  var number = [];
+  for (var i = 1; i <= limit; i++) {
+    if (i % 5 == 0) {
+      number.push(i);
+    } else {
+    }
+  }
+
+  return number;
+}
+
+console.log(divisibleFive(50));
+
+//Create a function that prints out 10 numbers that are multiple of 6.
+
+//start at 6
+//output is a list of 10 numbers separated by commas
+
+function multipleSix() {
+  var result = "";
+  for (var i = 1; i <= 10; i++) {
+    if (i != 10) {
+      result = result.concat(i * 6, ",");
+    } else {
+      result = result.concat(i * 6);
+    }
+  }
+
+  return result;
+}
+
+console.log(multipleSix());
